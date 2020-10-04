ece674fac6780527b347e6f9e7d56e14007483ea
Alexey <gamestol@mail.ru>
2020-10-04 23:09:44 +0300

    Lesson1

--- app/src/main/java/me/tolkstudio/myapplication/MainActivity.java
+++ app/src/main/java/me/tolkstudio/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package me.tolkstudio.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
+
+
+
 }
